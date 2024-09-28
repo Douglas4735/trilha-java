420aafbee947eb2c9fbb7f24336a7dbeaca33c25
douglas silva <douglas4735@gmail.com>
2024-09-28 16:56:40 -0400

    add tipo de dados

---
+++ tipos-variaveis/src/TipoDeDados.java
@@ -0,0 +1,14 @@
+public class TipoDeDados {
+    public static void main(String[] args) throws Exception {
+
+        byte idade = 127;
+        short ano = 2025;
+        int cep = 758658754;
+        long cpf = 25468795205L;
+        float pi = 3.14F;
+        double salario = 1000.25;
+
+
+        System.out.println(idade);
+    }
+}
