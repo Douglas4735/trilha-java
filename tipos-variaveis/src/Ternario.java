725c9869cba2f551726bf50165f3b1bdeef38e2c
douglas silva <douglas4735@gmail.com>
2024-09-28 18:17:27 -0400

    add ternario

---
+++ tipos-variaveis/src/Ternario.java
@@ -0,0 +1,19 @@
+public class Ternario {
+    public static void main(String[] args) {
+       
+
+        int a,b,c;
+
+        a=1;
+        b=2;
+        c=1;
+
+        String resultado = a==c ? "verdade" : "mentira";
+
+
+        System.out.println(resultado);
+
+
+    }
+    
+}
