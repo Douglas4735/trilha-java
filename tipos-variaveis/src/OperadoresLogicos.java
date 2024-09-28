bcefacb4baf61d124c93e7ee2137dc44b123cbbc
douglas silva <douglas4735@gmail.com>
2024-09-28 19:11:30 -0400

    add operadores-logicos

--- tipos-variaveis/src/OperadoresLogicos.java
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -1,10 +1,26 @@
 public class OperadoresLogicos {
     public static void main(String[] args) {
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && (7 > 8)) {
+            System.out.println("As duas condições são verdadeiras");
+        }else{
+            System.out.println("As duas condições são falsas");
+        }
         
-        boolean variavel = true;
+
+
+
+
+
+
+        /*  boolean variavel = true;
         variavel = !variavel;
 
-        System.out.println(variavel);
+        System.out.println(variavel);*/
+       
     }
     
 }
