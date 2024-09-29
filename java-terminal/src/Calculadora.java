4677bb6c71cff70705e670b36ba6992a1e3b3184
douglas silva <douglas4735@gmail.com>
2024-09-28 21:24:02 -0400

    criacao de um documento.

---
+++ java-terminal/src/Calculadora.java
@@ -0,0 +1,33 @@
+
+    /**
+        * <h1>Calculadora<h1>
+        * A Calculadora realiza operações matemáticas entre numeros inteiros 
+        * <p>
+        * <b>Note:</b> Leia atentamente a documentação dessa classe
+        * para desfrutar dos recursos oferecidos pelo autor
+        * 
+        * @author Douglas silva
+        * @version 1.0
+        * @since 28/09/24
+    */
+
+
+public class Calculadora {
+    /**
+     * @param args
+     * @throws Exception
+     */
+   
+    /**
+     * Este método é ultilizado para somar dois valores inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método éo resultado dos dois numeros.
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+        
+    //este é um modo de criar uma documentação via terminal =[ javadoc -encoding UTF-8 -d ocencoding ISO-8859-1 -d ../docs src/*.java ]
+    
+}
