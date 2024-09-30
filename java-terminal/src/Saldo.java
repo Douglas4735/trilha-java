9a49f9c0bcc039cffb452dc96e07a1424b8097f3
douglas silva <douglas4735@gmail.com>
2024-09-29 22:28:37 -0400

    Criado a media

---
+++ java-terminal/src/Saldo.java
@@ -0,0 +1,18 @@
+public class Saldo {
+    public static void main(String[] args) {
+        
+        double saldo = 25.0;
+        double valorSolicitado = 150.0;
+
+        if (valorSolicitado < saldo) {
+            saldo -= valorSolicitado;
+            System.out.println("Novo saldo = "+saldo);
+        }else{
+            System.out.println("O valor que você solicitou é: "+valorSolicitado);
+            System.out.println("Seu saldo é: "+saldo);
+
+        }
+
+    }
+    
+}
--- java-terminal/src/TreinoJava.java
+++ java-terminal/src/TreinoJava.java
@@ -1,19 +1,10 @@
 public class TreinoJava {
     public static void main(String[] args) {
         
-
-        double saldo = 25.0;
-        double valorSolicitado = 150.0;
-
-        if (valorSolicitado < saldo) {
-            saldo -= valorSolicitado;
-            System.out.println("Novo saldo = "+saldo);
-        }else{
-            System.out.println("O valor que você solicitou é: "+valorSolicitado);
-            System.out.println("Seu saldo é: "+saldo);
-
+        for(int carneirinho = 0; carneirinho <= 20; carneirinho++ ){
+            System.out.println(carneirinho);
         }
-
+            System.out.println("Joãozinho dormil.");
     }
     
 }
