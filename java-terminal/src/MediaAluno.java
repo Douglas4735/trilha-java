0e6abe0d316118b6b84b1374e699da7575eed61d
douglas silva <douglas4735@gmail.com>
2024-09-29 20:01:27 -0400

    add MediaAluno

---
+++ java-terminal/src/MediaAluno.java
@@ -0,0 +1,18 @@
+public class MediaAluno {
+
+    public static void main(String[] args) {
+        
+    double media = 4;
+
+    if(media >= 7){
+        System.out.println("APROVADO");
+    }else if(media >=5 && media < 7){
+        System.out.println("RECUPERAÇÃO");
+    }else{
+        System.out.println("REPROVADO");
+    }
+    System.out.println(media);
+    
+    }
+    
+}
